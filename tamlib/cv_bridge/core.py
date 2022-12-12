e08293f2fec425553b98a58546d46e0973903cd8
Tomohiro Ono <s1107ot@gmail.com>
2022-12-12 20:01:53 +0900

    [Add] error messages

--- tamlib/cv_bridge/core.py
+++ tamlib/cv_bridge/core.py
@@ -1,5 +1,4 @@
 import struct
-import sys
 
 import cv2
 import numpy as np
@@ -10,7 +9,7 @@ from sensor_msgs.msg import CompressedImage, Image
 
 
 class CvBridge:
-    def __init__(self):
+    def __init__(self) -> None:
         self.node_name = rospy.get_name()
         self.bridge = _CvBridge()
 
@@ -27,8 +26,9 @@ class CvBridge:
         try:
             cv_img = self.bridge.imgmsg_to_cv2(img_msg, encoding)
             return cv_img
-        except CvBridgeError:
-            rospy.logwarn(f"[{self.node_name}]: CvBridge FAILURE")
+        except CvBridgeError as e:
+            rospy.logwarn(f"[{self.node_name}]: {e}")
+            rospy.logwarn(f"[{self.node_name}]: CvBridge FAILURE.")
             return None
 
     def imgmsg_to_depth(self, img_msg: Image) -> np.ndarray:
@@ -58,8 +58,9 @@ class CvBridge:
         try:
             cv_img = self.bridge.compressed_imgmsg_to_cv2(img_msg, encoding)
             return cv_img
-        except CvBridgeError:
-            rospy.logwarn(f"[{self.node_name}]: CvBridge FAILURE")
+        except CvBridgeError as e:
+            rospy.logwarn(f"[{self.node_name}]: {e}")
+            rospy.logwarn(f"[{self.node_name}]: CvBridge FAILURE.")
             return None
 
     def compressed_imgmsg_to_depth(
@@ -79,33 +80,38 @@ class CvBridge:
         Returns:
             np.ndarray: cv2形式のDepth画像．
         """
-        encode, _type = img_msg.format.split(";")
-        encode = encode.strip()
-        _type = _type.strip()
-        if "compressedDepth" not in _type:
-            raise Exception("Compression type is not 'compressedDepth'.")
-        header_size = 12
-        raw_data = img_msg.data[header_size:]
+        try:
+            encode, _type = img_msg.format.split(";")
+            encode = encode.strip()
+            _type = _type.strip()
+            if "compressedDepth" not in _type:
+                raise Exception("Compression type is not 'compressedDepth'.")
+            header_size = 12
+            raw_data = img_msg.data[header_size:]
 
-        depth_img_raw = cv2.imdecode(
-            np.fromstring(raw_data, np.uint8), cv2.IMREAD_UNCHANGED
-        )
-        if depth_img_raw is None:
-            raise Exception("Could not decode compressed depth image.")
-
-        if encode == "16UC1":
-            return depth_img_raw
-        elif encode == "32FC1":
-            raw_header = img_msg.data[:header_size]
-            _, depth_quant_a, depth_quant_b = struct.unpack("iff", raw_header)
-            depth_img_scaled = depth_quant_a / (
-                depth_img_raw.astype(np.float32) - depth_quant_b
+            depth_img_raw = cv2.imdecode(
+                np.fromstring(raw_data, np.uint8), cv2.IMREAD_UNCHANGED
             )
-            depth_img_scaled[depth_img_raw == 0] = 0
-            depth_img_mm = (depth_img_scaled * 1000).astype(np.uint16)
-            return depth_img_mm
-        else:
-            raise Exception("Decoding of '" + encode + "' is not implemented.")
+            if depth_img_raw is None:
+                raise Exception("Could not decode compressed depth image.")
+
+            if encode == "16UC1":
+                return depth_img_raw
+            elif encode == "32FC1":
+                raw_header = img_msg.data[:header_size]
+                _, depth_quant_a, depth_quant_b = struct.unpack("iff", raw_header)
+                depth_img_scaled = depth_quant_a / (
+                    depth_img_raw.astype(np.float32) - depth_quant_b
+                )
+                depth_img_scaled[depth_img_raw == 0] = 0
+                depth_img_mm = (depth_img_scaled * 1000).astype(np.uint16)
+                return depth_img_mm
+            else:
+                raise Exception("Decoding of '" + encode + "' is not implemented.")
+        except Exception as e:
+            rospy.logwarn(f"[{self.node_name}]: {e}")
+            rospy.logwarn(f"[{self.node_name}]: CvBridge FAILURE.")
+            return None
 
     def cv2_to_imgmsg(self, cv_img: np.ndarray, encoding="passthrough") -> Image:
         """cv2形式の画像をImageメッセージに変換する
@@ -120,7 +126,8 @@ class CvBridge:
         try:
             img_msg = self.bridge.cv2_to_imgmsg(cv_img, encoding)
             return img_msg
-        except CvBridgeError:
+        except CvBridgeError as e:
+            rospy.logwarn(f"[{self.node_name}]: {e}")
             rospy.logwarn(f"[{self.node_name}]: CvBridge FAILURE")
             return None
 
@@ -140,6 +147,7 @@ class CvBridge:
         try:
             img_msg = self.bridge.cv2_to_compressed_imgmsg(cv_img, format)
             return img_msg
-        except CvBridgeError:
+        except CvBridgeError as e:
+            rospy.logwarn(f"[{self.node_name}]: {e}")
             rospy.logwarn(f"[{self.node_name}]: CvBridge FAILURE")
             return None
