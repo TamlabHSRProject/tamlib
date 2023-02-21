53d7f417db70abfa028da9bffc3e8ca9d34aaa31
haganeLego <hagane01061998@icloud.com>
2023-02-22 05:41:51 +0900

    [Update]setting independent

--- src/tamlib/utils/logger.py
+++ src/tamlib/utils/logger.py
@@ -1,17 +1,18 @@
 import sys
 from loguru import logger
 import rospy
+import copy
 
 class Logger:
     def __init__(self, logger_name=None, loglevel='INFO',
-                 format='<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green>  <level>{level: <8}</level> | {extra[logger_name]} |<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>',) -> None:
+                 format='<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | <level>{level: <8}</level> | {extra[logger_name]} |<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>',) -> None:
         if logger_name is None:
             self._logger_name = rospy.get_name()
         else:
-            self._logger_name = logger_name
-        self._logger = logger.bind(logger_name=self._logger_name)
-        self._logger.remove()
-        self._logger.add(sys.stdout, format=format, level=loglevel, backtrace=False, diagnose=False)
+            self._logger_name = logger_name 
+        logger.remove()
+        self._logger = copy.deepcopy(logger.bind(logger_name=self._logger_name))
+        self._logger.add(sys.stdout, format=format, level=loglevel)
 
     def start_dump(self, path: str, level='DEBUG'):
         self._logger.add(path, level=level,
