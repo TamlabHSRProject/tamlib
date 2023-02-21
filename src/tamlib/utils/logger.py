c542cb7267b12f95c2c35179194938169fa8c21f
haganeLego <hagane01061998@icloud.com>
2023-02-22 06:01:10 +0900

    [Add]docstring

--- src/tamlib/utils/logger.py
+++ src/tamlib/utils/logger.py
@@ -6,6 +6,17 @@ import copy
 class Logger:
     def __init__(self, logger_name=None, loglevel='INFO',
                  format='<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | <level>{level: <8}</level> | {extra[logger_name]} |<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>',) -> None:
+        """Initialize function
+
+        Parameters
+        ----------
+        logger_name : str, optional
+            logger_name. When not set, get from rospy.get_name() , by default None
+        loglevel : str, optional
+            standard output log level, by default 'INFO'
+        format : str, optional
+            log format, by default '<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | <level>{level: <8}</level> | {extra[logger_name]} |<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>'
+        """
         if logger_name is None:
             self._logger_name = rospy.get_name()
         else:
@@ -14,21 +25,29 @@ class Logger:
         self._logger = copy.deepcopy(logger.bind(logger_name=self._logger_name))
         self._logger.add(sys.stdout, format=format, level=loglevel)
 
-    def start_dump(self, path: str, level='DEBUG'):
-        self._logger.add(path, level=level,
-                         filter=lambda record: record["extra"]["logger_name"] == self._logger_name)
+    def start_dump(self, path: str, loglevel='DEBUG'):
+        """function to start dump log to file
+
+        Parameters
+        ----------
+        path : str
+            logfile path
+        loglevel : str, optional
+            dump log level, by default 'DEBUG'
+        """
+        self._logger.add(path, level=loglevel)
 
     def logdebug(self, message) -> None:
         self._logger.opt(depth=1).debug(message)
 
-    def loginfo(self, message) -> None:
+    def loginfo(self, message: str) -> None:
         self._logger.opt(depth=1).info(message)
 
-    def logwarn(self, message) -> None:
+    def logwarn(self, message: str) -> None:
         self._logger.opt(depth=1).warning(message)
 
-    def logerr(self, message) -> None:
+    def logerr(self, message: str) -> None:
         self._logger.opt(depth=1).error(message)
 
-    def logfatal(self, message) -> None:
+    def logfatal(self, message: str) -> None:
         self._logger.opt(depth=1).critical(message)
